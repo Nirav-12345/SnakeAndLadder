f964e28acbe7a70f9d0315d67880c66ebd6a8a96
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-22 10:27:06 -0800

    Adding filke

---
+++ RollingDiceSnLa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingnumberRolls
+{
+    internal class RollingDiceSnLa
+    {
+
+        public static void RollsTheDie()
+
+        {
+           
+
+            Random random = new Random();
+        int DiceRoll = random.Next(1, 7);
+
+        Console.WriteLine("The Number is :" + DiceRoll);
+            
+        }
+}
+    }
+
