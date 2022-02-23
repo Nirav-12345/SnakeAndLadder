ee3970c91985a4c29efe2eb86dd2657b47aaf619
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-23 10:00:45 -0800

    adding program file

---
+++ WinPos2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPos2
+{
+    public class Winpos2
+    {
+        public static void PlayGame()
+        {
+            const int No_Play = 0, Ladder = 1, Snake = 2, Winning_Posi = 100;
+            int player_Posi = 0, DiceRoll;
+
+            Console.WriteLine("Start the game");
+            Console.WriteLine("Starting position player is: " + player_Posi);
+            Random random = new Random();
+
+            while (player_Posi < Winning_Posi)
+            {
+                DiceRoll = random.Next(1, 7);
+                Console.WriteLine("Player get Dice Number " + DiceRoll);
+
+                switch (random.Next(0, 3))
+                {
+                    case No_Play:
+                        Console.WriteLine(" Player Position " + player_Posi);
+                        break;
+
+                    case Ladder:
+                        player_Posi += DiceRoll;
+                        if (player_Posi > Winning_Posi)
+                        {
+                            player_Posi -= DiceRoll;
+                        }
+                        Console.WriteLine("Ladder Player Position " + player_Posi);
+                        break;
+                    case Snake:
+                        player_Posi -= DiceRoll;
+                        if (player_Posi < 0)
+                        {
+                            player_Posi = 0;
+                        }
+                        Console.WriteLine("Snake Player Position " + player_Posi);
+                        break;
+                }
+                Console.WriteLine(" Congratulations you Wi");
+            }
+
+        }
+    }
+}
